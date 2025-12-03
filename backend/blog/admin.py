6ccc0ef95929bf2f3aa895c436156fee7386403a
apo-cyber <poirot1948@gmail.com>
2025-12-03 22:36:13 +0900

    Hide User and Group from admin sidebar

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,44 +1,40 @@
 # blog/admin.py
 
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 from .models import BlogPost, Tag
+
+# 認証と認可セクションを非表示
+admin.site.unregister(User)
+admin.site.unregister(Group)
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     """タグ管理画面の設定"""
-    list_display = ['name', 'created_at']
-    search_fields = ['name']
-    ordering = ['name']
+
+    list_display = ["name", "created_at"]
+    search_fields = ["name"]
+    ordering = ["name"]
 
 
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
     """ブログ記事管理画面の設定"""
-    list_display = ['title', 'is_published', 'created_at', 'updated_at']
-    list_filter = ['is_published', 'created_at', 'tags']
-    search_fields = ['title', 'description']
-    filter_horizontal = ['tags']
-    date_hierarchy = 'created_at'
-    readonly_fields = ['created_at', 'updated_at']
+
+    list_display = ["title", "is_published", "created_at", "updated_at"]
+    list_filter = ["is_published", "created_at", "tags"]
+    search_fields = ["title", "description"]
+    filter_horizontal = ["tags"]
+    date_hierarchy = "created_at"
+    readonly_fields = ["created_at", "updated_at"]
 
     fieldsets = (
-        ('基本情報', {
-            'fields': ('title', 'description')
-        }),
-        ('画像', {
-            'fields': ('image',)
-        }),
-        ('タグ', {
-            'fields': ('tags',)
-        }),
-        ('公開設定', {
-            'fields': ('is_published', 'published_at')
-        }),
-        ('タイムスタンプ', {
-            'fields': ('created_at', 'updated_at'),
-            'classes': ('collapse',)
-        }),
+        ("基本情報", {"fields": ("title", "description")}),
+        ("画像", {"fields": ("image",)}),
+        ("タグ", {"fields": ("tags",)}),
+        ("公開設定", {"fields": ("is_published", "published_at")}),
+        ("タイムスタンプ", {"fields": ("created_at", "updated_at"), "classes": ("collapse",)}),
     )
 
     def save_model(self, request, obj, form, change):
